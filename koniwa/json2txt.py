164d6855b0c3ad3d762d262491735c0498884c48
Yuta Hayashibe <yuta@hayashibe.jp>
2024-06-05 16:43:52 +0900

    Fix lint

--- koniwa/json2txt.py
+++ koniwa/json2txt.py
@@ -3,10 +3,10 @@
 import argparse
 from pathlib import Path
 
-from schema import Data
+from koniwa.schema import Data
 
 
-def operation(path_in: Path, path_out: Path) -> None:
+def operation(path_in: Path) -> None:
     d = Data.parse_file(path_in)
     part = []
     for span in d.annotation:
@@ -18,13 +18,12 @@ def operation(path_in: Path, path_out: Path) -> None:
 def get_opts() -> argparse.Namespace:
     oparser = argparse.ArgumentParser()
     oparser.add_argument("--input", "-i", type=Path, default="/dev/stdin", required=False)
-    oparser.add_argument("--output", "-o", type=Path, default="/dev/stdout", required=False)
     return oparser.parse_args()
 
 
 def main() -> None:
     opts = get_opts()
-    operation(opts.input, opts.output)
+    operation(opts.input)
 
 
 if __name__ == "__main__":
